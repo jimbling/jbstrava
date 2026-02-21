5670336474325d7274ee9e2b1fbfb29ba9ea550c
jimbling <jimbling05@gmail.com>
2026-02-22 06:47:51 +0700

    test job redis1

--- app/Jobs/TestRedisJob.php
+++ app/Jobs/TestRedisJob.php
@@ -2,13 +2,17 @@
 
 namespace App\Jobs;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-
-
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 class TestRedisJob implements ShouldQueue
 {
-    public function handle()
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle(): void
     {
         sleep(60);
     }
